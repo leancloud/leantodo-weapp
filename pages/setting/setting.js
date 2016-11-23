27607ddecb4bc0b491b77d84cfd89bceebdaf0be
leeyeh <lee55962698@gmail.com>
2016-11-23 18:35:36 +0800

    fix: update for weapp v0.11.112200
    - move AV.init to Page.onLaunch
    - clean up and indent fix

--- pages/setting/setting.js
+++ pages/setting/setting.js
@@ -1,48 +1,52 @@
-const AV = require('../../utils/leancloud-storage');
-const Todo = require('../../todo');
+const { User } = require('../../utils/leancloud-storage');
 
 Page({
-    data: {
-        user: AV.User.current(),
-        username: '',
-        password: '',
-        error: null,
-    },
-    updateUsername:  function ({
-        detail: {
-            value
-        }
-    }) {
-        this.setData({
-            username: value
-        });
-    },
-    updatePassword:  function ({
-        detail: {
-            value
-        }
-    }) {
-        this.setData({
-            password: value
-        });
-    },
-    save: function() {
-        this.setData({
-            error: null,
-        });
-        const { username, password } = this.data;
-        const user = AV.User.current();
-        if (username) user.set({username});
-        if (password) user.set({password});
-        user.save().then(() => {
-            wx.showToast({
-                title: '更新成功',
-                icon: 'success',
-            });
-        }).catch(error => {
-            this.setData({
-                error: error.message,
-            });
-        });
+  data: {
+    user: null,
+    username: '',
+    password: '',
+    error: null,
+  },
+  onLoad: function() {
+    this.setData({
+      user: User.current(),
+    });
+  },
+  updateUsername: function ({
+    detail: {
+      value
     }
+  }) {
+    this.setData({
+      username: value
+    });
+  },
+  updatePassword: function ({
+    detail: {
+      value
+    }
+  }) {
+    this.setData({
+      password: value
+    });
+  },
+  save: function () {
+    this.setData({
+      error: null,
+    });
+    const { username, password } = this.data;
+    const user = User.current();
+    if (username) user.set({ username });
+    if (password) user.set({ password });
+    user.save().then(() => {
+      wx.showToast({
+        title: '更新成功',
+        icon: 'success',
+      });
+    }).catch(error => {
+      this.setData({
+        error: error.message,
+      });
+    });
+  }
 });
--- pages/todos/todos.js
+++ pages/todos/todos.js
@@ -1,129 +1,138 @@
 const AV = require('../../utils/leancloud-storage');
-const Todo = require('../../todo');
+const Todo = require('../../model/todo');
 
 Page({
-    data: {
-        todos: [],
-        editedTodo: null,
-        draft: '',
-        editDraft: null,
-    },
-    loginAndFetchTodos: function() {
-        return Promise.resolve(AV.User.current()).then(user =>
-            user ? (user.isAuthenticated().then(authed => authed ? user : null)) : null
-        ).then(user =>
-            user ? user : AV.User.loginWithWeapp()
-        ).then((user) => {
-            console.log('uid', user.id);
-            return new AV.Query(Todo)
-                .equalTo('user', AV.Object.createWithoutData('User', user.id))
-                .descending('createdAt')
-                .find()
-                .then(this.setTodos);
-        }).catch(error => console.error(error.message));
-    },
-    onLoad: function () {
-        this.loginAndFetchTodos();
-    },
-    onPullDownRefresh: function() {
-        this.loginAndFetchTodos().then(wx.stopPullDownRefresh);
-    },
-    setTodos: function(todos) {
-        const activeTodos = todos.filter(todo => !todo.done);
-        this.setData({
-            todos,
-            activeTodos,
-        });
-    },
-    updateDraft: function ({
-        detail: {
-            value
-        }
-    }) {
-        this.setData({
-            draft: value
-        });
-    },
-    addTodo: function () {
-        var value = this.data.draft && this.data.draft.trim()
-        if (!value) {
-            return;
-        }
-        var acl = new AV.ACL();
-        acl.setPublicReadAccess(false);
-        acl.setPublicWriteAccess(false);
-        acl.setReadAccess(AV.User.current(), true);
-        acl.setWriteAccess(AV.User.current(), true);
-        new Todo({
-            content: value,
-            done: false,
-            user: AV.User.current()
-        }).setACL(acl).save().then((todo) => {
-            this.setTodos([todo, ...this.data.todos]);
-        }).catch(console.error);
-        this.setData({
-          draft: ''
-        });
-    },
-    toggleDone: function({
-        target: {
-            dataset: {
-                id
-            }
-        }
-    }) {
-        const { todos } = this.data;
-        const currentTodo = todos.filter(todo => todo.id === id)[0];
-        currentTodo.done = !currentTodo.done;
-        currentTodo.save()
-            .then(() => this.setTodos(todos))
-            .catch(console.error);
-    },
-    editTodo: function ({
-        target: {
-            dataset: {
-                id
-            }
-        }
-    }) {
-        this.setData({
-            editDraft: null,
-            editedTodo: this.data.todos.filter(todo => todo.id === id)[0]
-        });
-    },
-    updateEditedContent: function ({
-        detail: {
-            value
-        }
-    }) {
-        this.setData({
-            editDraft: value
-        });
-    },
-    doneEdit: function ({
-        target: {
-            dataset: {
-                id
-            }
-        }
-    }) {
-        const { todos, editDraft } = this.data;
-        if (editDraft === null) return;
-        const currentTodo = todos.filter(todo => todo.id === id)[0];
-        if (editDraft === currentTodo.content) return;
-        currentTodo.content = editDraft;
-        currentTodo.save().then(() => {
-            this.setTodos(todos);
-        }).catch(console.error);
-    },
-    removeDone: function() {
-        AV.Object.destroyAll(this.data.todos.filter(todo => todo.done)).then(() => {
-            this.setTodos(this.data.activeTodos);
-        }).catch(console.error);
-    },
-    setting: function() {
-        wx.navigateTo({
-          url: '../setting/setting',
-        });
+  data: {
+    todos: [],
+    editedTodo: {},
+    draft: '',
+    editDraft: null,
+  },
+  loginAndFetchTodos: function () {
+    return AV.Promise.resolve(AV.User.current()).then(user =>
+      user ? (user.isAuthenticated().then(authed => authed ? user : null)) : null
+    ).then(user =>
+      user ? user : AV.User.loginWithWeapp()
+    ).then((user) => {
+      console.log('uid', user.id);
+      return new AV.Query(Todo)
+        .equalTo('user', AV.Object.createWithoutData('User', user.id))
+        .descending('createdAt')
+        .find()
+        .then(this.setTodos)
+        .catch(console.error);
+    }).catch(error => console.error(error.message));
+  },
+  onReady: function() {
+    console.log('page ready');
+    this.loginAndFetchTodos();
+  },
+  onLoad: function () {
+    console.log('page load')
+    AV.init({
+      appId: 'ozewwcwsyq92g2hommuxqrqzg6847wgl8dtrac6suxzko333',
+      appKey: 'ni0kwg7h8hwtz6a7dw9ipr7ayk989zo5y8t0sn5gjiel6uav',
+    });
+  },
+  onPullDownRefresh: function () {
+    this.loginAndFetchTodos().then(wx.stopPullDownRefresh);
+  },
+  setTodos: function (todos) {
+    const activeTodos = todos.filter(todo => !todo.done);
+    this.setData({
+      todos,
+      activeTodos,
+    });
+  },
+  updateDraft: function ({
+    detail: {
+      value
     }
-})
+  }) {
+    this.setData({
+      draft: value
+    });
+  },
+  addTodo: function () {
+    var value = this.data.draft && this.data.draft.trim()
+    if (!value) {
+      return;
+    }
+    var acl = new AV.ACL();
+    acl.setPublicReadAccess(false);
+    acl.setPublicWriteAccess(false);
+    acl.setReadAccess(AV.User.current(), true);
+    acl.setWriteAccess(AV.User.current(), true);
+    new Todo({
+      content: value,
+      done: false,
+      user: AV.User.current()
+    }).setACL(acl).save().then((todo) => {
+      this.setTodos([todo, ...this.data.todos]);
+    }).catch(console.error);
+    this.setData({
+      draft: ''
+    });
+  },
+  toggleDone: function ({
+    target: {
+      dataset: {
+        id
+      }
+    }
+  }) {
+    const { todos } = this.data;
+    const currentTodo = todos.filter(todo => todo.id === id)[0];
+    currentTodo.done = !currentTodo.done;
+    currentTodo.save()
+      .then(() => this.setTodos(todos))
+      .catch(console.error);
+  },
+  editTodo: function ({
+    target: {
+      dataset: {
+        id
+      }
+    }
+  }) {
+    this.setData({
+      editDraft: null,
+      editedTodo: this.data.todos.filter(todo => todo.id === id)[0] || {}
+    });
+  },
+  updateEditedContent: function ({
+    detail: {
+      value
+    }
+  }) {
+    this.setData({
+      editDraft: value
+    });
+  },
+  doneEdit: function ({
+    target: {
+      dataset: {
+        id
+      }
+    }
+  }) {
+    const { todos, editDraft } = this.data;
+    if (editDraft === null) return;
+    const currentTodo = todos.filter(todo => todo.id === id)[0];
+    if (editDraft === currentTodo.content) return;
+    currentTodo.content = editDraft;
+    currentTodo.save().then(() => {
+      this.setTodos(todos);
+    }).catch(console.error);
+  },
+  removeDone: function () {
+    AV.Object.destroyAll(this.data.todos.filter(todo => todo.done)).then(() => {
+      this.setTodos(this.data.activeTodos);
+    }).catch(console.error);
+  },
+  setting: function () {
+    wx.navigateTo({
+      url: '../setting/setting',
+    });
+  },
+});
