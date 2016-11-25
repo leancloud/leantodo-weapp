41fcd3614be5612cbc40300f9872b9b5749ede39
leeyeh <lee55962698@gmail.com>
2016-11-25 10:39:37 +0800

    fix: revert patch for weapp v0.11.112200

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const AV = require('./utils/leancloud-storage');
 
-//app.js
-App({
-  onLaunch: function() {
-    console.log('app launch')
-  }
+AV.init({
+  appId: 'ozewwcwsyq92g2hommuxqrqzg6847wgl8dtrac6suxzko333',
+  appKey: 'ni0kwg7h8hwtz6a7dw9ipr7ayk989zo5y8t0sn5gjiel6uav',
 });
+
+//app.js
+App({});
--- pages/todos/todos.js
+++ pages/todos/todos.js
@@ -27,13 +27,6 @@ Page({
     console.log('page ready');
     this.loginAndFetchTodos();
   },
-  onLoad: function () {
-    console.log('page load')
-    AV.init({
-      appId: 'ozewwcwsyq92g2hommuxqrqzg6847wgl8dtrac6suxzko333',
-      appKey: 'ni0kwg7h8hwtz6a7dw9ipr7ayk989zo5y8t0sn5gjiel6uav',
-    });
-  },
   onPullDownRefresh: function () {
     this.loginAndFetchTodos().then(wx.stopPullDownRefresh);
   },
